64f7c1b3b6baa986d7af509ce2e8e7bebded54d6
Perrine Chaissac <perrine.chaissac@ecedi.fr>
2025-01-28 12:00:53 +0100

    Ref #100171 - JS icon menu canteen

---
+++ assets/js/icon-menu-canteen.js
@@ -0,0 +1,36 @@
+window.rennes = window.rennes || {};
+
+window.rennes.MenuCanteen = function () {
+  this.menus = document.querySelectorAll('.block-class-canteen-menu');
+
+  const labels = ["[BIO]", "[FR]", "[LOC]", "[VEGE]", "[DUR]", "[UEL]", "[UEF]"];
+
+  this.menus.forEach((menu) => {
+    const items = menu.querySelectorAll('td');
+
+    items.forEach((item) => {
+      let itemContent = item.textContent;
+
+      labels.forEach((label) => {
+        if (itemContent.includes(label)) {
+          itemContent = itemContent.replace(label, '').trim();
+          item.innerHTML = '';
+          const span = document.createElement('span');
+          span.textContent = itemContent;
+          item.appendChild(span);
+
+          const cleanLabel = label.replace(/\[|\]/g, '');
+          const img = document.createElement('img');
+          img.src = `/assets/images/icons/${cleanLabel}.png`;
+          img.alt = cleanLabel;
+
+          item.appendChild(img);
+        }
+      });
+    });
+  });
+};
+
+document.addEventListener('DOMContentLoaded', function () {
+  new window.rennes.MenuCanteen();
+});
