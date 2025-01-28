076b0d972e5a79710ec9950044ad09d7b6f5f1a6
Perrine Chaissac <perrine.chaissac@ecedi.fr>
2025-01-28 12:07:56 +0100

    Ref #100171 - add class condition

--- assets/js/icon-menu-canteen.js
+++ assets/js/icon-menu-canteen.js
@@ -1,36 +1,37 @@
 window.rennes = window.rennes || {};
 
 window.rennes.MenuCanteen = function () {
-  this.menus = document.querySelectorAll('.block-class-canteen-menu');
+    this.menus = document.querySelectorAll('.block-class-canteen-menu');
+    if (this.menus.length > 0) {
+        const labels = ["[BIO]", "[FR]", "[LOC]", "[VEGE]", "[DUR]", "[UEL]", "[UEF]"];
 
-  const labels = ["[BIO]", "[FR]", "[LOC]", "[VEGE]", "[DUR]", "[UEL]", "[UEF]"];
+        this.menus.forEach((menu) => {
+            const items = menu.querySelectorAll('td, legend');
 
-  this.menus.forEach((menu) => {
-    const items = menu.querySelectorAll('td, legend');
+            items.forEach((item) => {
+                let itemContent = item.textContent;
 
-    items.forEach((item) => {
-      let itemContent = item.textContent;
+                labels.forEach((label) => {
+                    if (itemContent.includes(label)) {
+                        itemContent = itemContent.replace(label, '').trim();
+                        item.innerHTML = '';
+                        const span = document.createElement('span');
+                        span.textContent = itemContent;
+                        item.appendChild(span);
 
-      labels.forEach((label) => {
-        if (itemContent.includes(label)) {
-          itemContent = itemContent.replace(label, '').trim();
-          item.innerHTML = '';
-          const span = document.createElement('span');
-          span.textContent = itemContent;
-          item.appendChild(span);
+                        const cleanLabel = label.replace(/\[|\]/g, '');
+                        const img = document.createElement('img');
+                        img.src = `/assets/images/icons/${cleanLabel}.png`;
+                        img.alt = cleanLabel;
 
-          const cleanLabel = label.replace(/\[|\]/g, '');
-          const img = document.createElement('img');
-          img.src = `/assets/images/icons/${cleanLabel}.png`;
-          img.alt = cleanLabel;
-
-          item.appendChild(img);
-        }
-      });
-    });
-  });
+                        item.appendChild(img);
+                    }
+                });
+            });
+        });
+    }
 };
 
 document.addEventListener('DOMContentLoaded', function () {
-  new window.rennes.MenuCanteen();
+    new window.rennes.MenuCanteen();
 });
