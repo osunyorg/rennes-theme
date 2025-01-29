b39dfc86cf26004761e79c2b92bc57ec3df9ca86
Malika HANOTTE <malika.hanotte@ecedi.fr>
2025-01-29 19:37:59 +0100

    Ref #100181 - Header desktop

--- assets/js/posts-sidebar.js
+++
@@ -1,9 +0,0 @@
-// import MainMenu from "osuny/assets/js/theme/design-system/mainMenu";
-//
-// class MainMenuBis extends MainMenu {
-//   updateUpperMenuPosition() {
-//     console.log('surcharge')
-//   }
-// }
-//
-// export default new MainMenuBis('header#document-header');
