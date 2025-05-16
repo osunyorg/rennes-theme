d2923f0b6af633926212e0778fdcaa75ee425fe7
Malika HANOTTE <malika.hanotte@ecedi.fr>
2025-05-16 08:50:14 +0200

    Ref #103611 - Adjust search-news placeholder

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,11 @@
+// Fichiers du thème Osuny nécessaire à son bon fonctionnement
 import './theme/';
+
+// Customisation
 import './icon-nav/';
 import './canteen-menu/';
 import './custom-iframe-map';
+import './search-override';
 
 document.addEventListener('DOMContentLoaded', function () {
     new window.rennes.MenuCanteen();
---
+++ assets/js/search-override.js
@@ -0,0 +1,6 @@
+window.osuny = window.osuny || {}
+window.osuny.pagefindOptions = {
+  translations: {
+    placeholder: "Rechercher une actualité",
+  }
+}
