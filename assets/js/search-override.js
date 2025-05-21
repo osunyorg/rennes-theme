60f03b027f66d59cb447b4495853c45122a1d726
Malika HANOTTE <malika.hanotte@ecedi.fr>
2025-05-21 18:15:33 +0200

    Ref #103611 - News search - button + title + form aria-label

--- assets/js/search-override.js
+++ assets/js/search-override.js
@@ -2,5 +2,6 @@ window.osuny = window.osuny || {}
 window.osuny.pagefindOptions = {
   translations: {
     placeholder: "Rechercher une actualité",
+    search_label: "Rechercher une actualité",
   }
 }
