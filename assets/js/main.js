232f5ef766371b46f5695f16431fea0e2f1d41fc
Arnaud Levy <contact@arnaudlevy.com>
2026-02-26 18:12:20 +0100

    Correct pour Ici Rennes

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,13 +4,11 @@ import './theme/';
 // Customisation
 import './orejime-config/';
 import './orejime-additional/';
-import './icon-nav/';
 import './canteen-menu/';
 import './custom-iframe-map';
 
 document.addEventListener('DOMContentLoaded', function () {
     new window.rennes.MenuCanteen();
-    new window.rennes.IconNav();
     new window.rennes.CustomIframeMap();
     new window.rennes.OrejimeUpdateVideo();
     new window.rennes.OrejimeConsentModalLink();
